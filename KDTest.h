41b27c1d076e4fee7e1dce08bdcb7e3e94a7eb77
marcogallen <marco.gallen@gmail.com>
2014-05-27 22:15:13 -0500

    added test class

---
+++ KDTest.h
@@ -0,0 +1,13 @@
+//
+//  KDTest.h
+//  GitTest
+//
+//  Created by Marco Antonio Gallen vargas on 27/05/14.
+//  Copyright (c) 2014 Kito Dynamics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KDTest : NSObject
+
+@end
---
+++ KDTest.m
@@ -0,0 +1,13 @@
+//
+//  KDTest.m
+//  GitTest
+//
+//  Created by Marco Antonio Gallen vargas on 27/05/14.
+//  Copyright (c) 2014 Kito Dynamics. All rights reserved.
+//
+
+#import "KDTest.h"
+
+@implementation KDTest
+
+@end
